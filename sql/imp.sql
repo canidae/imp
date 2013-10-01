83371b1754fc05beb02ad98a9c65e010b658e82b
Vidar Wahlberg <canidae@exent.net>
2013-10-01 22:57:19 +0200

    and a simple ddl for the database

---
+++ sql/imp.sql
@@ -0,0 +1,187 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: member; Type: TABLE; Schema: public; Owner: imp; Tablespace: 
+--
+
+CREATE TABLE member (
+    member_id integer NOT NULL,
+    loginname character varying(256) NOT NULL,
+    password character varying(512) NOT NULL
+);
+
+
+ALTER TABLE public.member OWNER TO imp;
+
+--
+-- Name: member_member_id_seq; Type: SEQUENCE; Schema: public; Owner: imp
+--
+
+CREATE SEQUENCE member_member_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.member_member_id_seq OWNER TO imp;
+
+--
+-- Name: member_member_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: imp
+--
+
+ALTER SEQUENCE member_member_id_seq OWNED BY member.member_id;
+
+
+--
+-- Name: track; Type: TABLE; Schema: public; Owner: imp; Tablespace: 
+--
+
+CREATE TABLE track (
+    track_id integer NOT NULL,
+    member_id integer NOT NULL,
+    artist character varying(512) NOT NULL,
+    title character varying(512) NOT NULL
+);
+
+
+ALTER TABLE public.track OWNER TO imp;
+
+--
+-- Name: track_track_id_seq; Type: SEQUENCE; Schema: public; Owner: imp
+--
+
+CREATE SEQUENCE track_track_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.track_track_id_seq OWNER TO imp;
+
+--
+-- Name: track_track_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: imp
+--
+
+ALTER SEQUENCE track_track_id_seq OWNED BY track.track_id;
+
+
+--
+-- Name: member_id; Type: DEFAULT; Schema: public; Owner: imp
+--
+
+ALTER TABLE ONLY member ALTER COLUMN member_id SET DEFAULT nextval('member_member_id_seq'::regclass);
+
+
+--
+-- Name: track_id; Type: DEFAULT; Schema: public; Owner: imp
+--
+
+ALTER TABLE ONLY track ALTER COLUMN track_id SET DEFAULT nextval('track_track_id_seq'::regclass);
+
+
+--
+-- Data for Name: member; Type: TABLE DATA; Schema: public; Owner: imp
+--
+
+COPY member (member_id, loginname, password) FROM stdin;
+1	canidae	supersecretshasum
+\.
+
+
+--
+-- Name: member_member_id_seq; Type: SEQUENCE SET; Schema: public; Owner: imp
+--
+
+SELECT pg_catalog.setval('member_member_id_seq', 1, true);
+
+
+--
+-- Data for Name: track; Type: TABLE DATA; Schema: public; Owner: imp
+--
+
+COPY track (track_id, member_id, artist, title) FROM stdin;
+1	1	artist	title
+2	1	artist	title
+3	1	artist	title
+4	1	artist	title
+5	1	artist	title
+\.
+
+
+--
+-- Name: track_track_id_seq; Type: SEQUENCE SET; Schema: public; Owner: imp
+--
+
+SELECT pg_catalog.setval('track_track_id_seq', 5, true);
+
+
+--
+-- Name: member_pkey; Type: CONSTRAINT; Schema: public; Owner: imp; Tablespace: 
+--
+
+ALTER TABLE ONLY member
+    ADD CONSTRAINT member_pkey PRIMARY KEY (member_id);
+
+
+--
+-- Name: track_pkey; Type: CONSTRAINT; Schema: public; Owner: imp; Tablespace: 
+--
+
+ALTER TABLE ONLY track
+    ADD CONSTRAINT track_pkey PRIMARY KEY (track_id);
+
+
+--
+-- Name: track_member_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: imp
+--
+
+ALTER TABLE ONLY track
+    ADD CONSTRAINT track_member_id_fkey FOREIGN KEY (member_id) REFERENCES member(member_id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
